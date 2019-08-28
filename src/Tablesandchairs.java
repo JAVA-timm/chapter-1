64e3226f7165d69e9f97f91015e6e070031d900d
brantimm696 <54542451+brantimm696@users.noreply.github.com>
2019-08-28 09:29:23 -0500

    update newest project

---
+++ src/Tablesandchairs.java
@@ -0,0 +1,35 @@
+public class Tablesandchairs
+{
+    public static void main(String[] args)
+    {
+        System.out.println("C                                                                             C");
+        System.out.println("C                                                                             C");
+        System.out.println("C                                                                             C");
+        System.out.println("C                                                                             C");
+        System.out.println("C                                                                             C");
+        System.out.println("C                                                                             C");
+        System.out.println("C                                                                             C");
+        System.out.println("C                                                                             C");
+        System.out.println("C                                                                             C");
+        System.out.println("C                                                                             C");
+        System.out.println("C                                                                             C");
+        System.out.println("C                                                                             C");
+        System.out.println("C                                                                             C");
+        System.out.println("C                                                                             C");
+        System.out.println("C                                                                             C");
+        System.out.println("C                                                                             C");
+        System.out.println("SEATSEATSEATSEATSEAT                TableTableTable            SEATSEATSEATSEAT");
+        System.out.println("L                  L                L             L            L              L");
+        System.out.println("L                  L                l             l            L              L");
+        System.out.println("L                  L                L             L            L              L");
+        System.out.println("L                  L                l             l            L              L");
+        System.out.println("L                  L                L             L            L              L");
+        System.out.println("L                  L                l             l            L              L");
+        System.out.println("L                  L                L             L            L              L");
+        System.out.println("L                  L                l             l            L              L");
+        System.out.println("L                  L                L             L            L              L");
+        System.out.println("L                  L                l             l            L              L");
+        System.out.println("L                  L                L             L            L              L");
+        System.out.println("L                  L                l             l            L              L");
+    }
+}
---
+++ src/TacoClass.java
@@ -0,0 +1,26 @@
+public class TacoClass
+{
+    public static void main(String[] args)
+    {
+        System.out.println("TRUCKTRUCKTRUCKTRUCKTRUCKTRUCKTRUCKTRUCKTRUCKTRUCKTRUCK")
+        System.out.println("T                TACO                                 T");
+        System.out.println("T                TRUCK                                T");
+        System.out.println("T                TACO                                 T");
+        System.out.println("T                TRUCK                                T");
+        System.out.println("T                TACO                                 T");
+        System.out.println("T                TRUCK                                T");
+        System.out.println("T                TACO                                 T");
+        System.out.println("T                TRUCK                                T");
+        System.out.println("T                TACO                                 T");
+        System.out.println("T                TRUCK                                T");
+        System.out.println("T                TACO                                 T");
+        System.out.println("T                TRUCK                                T");
+        System.out.println("T                                                     TRUCKTRUCKTRUCKTRUCKTRUCK");
+        System.out.println("T  ---------------              -----------------     TRUCKTRUCKTRUCKTRUCKTRUCK");
+        System.out.println("T  000000000000  -              - 000000000000  -     TRUCKTRUCKTRUCKTRUCKTRUCK");
+        System.out.println("T  0          0  -              - 0          0  -     TRUCKTRUCKTRUCKTRUCKTRUCK");
+        System.out.println("T  0          0  - -------------- 0          0 ------ TRUCKTRUCKTRUCKTRUCKTRUCK");
+        System.out.println("   0          0                   0          0")
+        System.out.println("   000000000000                   000000000000")
+    }
+}
