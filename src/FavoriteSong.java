c4882db2bf9d1e3b578afac605d47e0e4175ef8a
brantimm696 <54542451+brantimm696@users.noreply.github.com>
2019-08-29 09:38:41 -0500

    update files

--- src/FavoriteSong.java
+++ src/FavoriteSong.java
@@ -1,2 +1,7 @@
-public class FavoriteSong {
+public class FavoriteSong
+{
+    public static void main(String[] args)
+    {
+        System.out.println("And the sign says anybody caught trespassing will be shot on sight So I jumped the fence and I yelled at the house, hey! what gives you the right To put up a fence and keep me out or to keep mother nature inIf god was here he'd tell it to your face man you're some kind of sinner");
+    }
 }
